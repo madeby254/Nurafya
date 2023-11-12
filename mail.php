678161ac52cc50b929eee9290194d4405f2b5375
madeby254 <mojitsu185@gmail.com>
2023-11-12 11:14:50 -0800

    Meow

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "wprealizer@gmail.com";
+        $recipient = "oldroot07@gmail.com";
 
         //Set the email subject.
         $subject = "New contact from $name";
